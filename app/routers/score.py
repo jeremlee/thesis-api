e42412631d25eb15e90eca3c7ff2d633dcb6c3b6
✿ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2025-09-23 22:36:46 +0800

    feat: Integrate cosine similarity

--- app/routers/score.py
+++ app/routers/score.py
@@ -20,7 +20,7 @@ router = APIRouter(prefix="/score", tags=["Score"])
 def cosine_similarity_scoring(resume_skills: List[str], job_skills: List[str]):
     corpus: list[str] = [" ".join(resume_skills), " ".join(job_skills)]
     vectorizer = TfidfVectorizer()
-    tfidf_matrix: csr_matrix = vectorizer.fit_transform(corpus) # type: ignore
+    tfidf_matrix: csr_matrix = vectorizer.fit_transform(corpus)  # type: ignore
     original_score = sk_cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
     scaled_score = 1 + 4 * original_score
     return scaled_score
@@ -78,8 +78,30 @@ async def score_candidate(user_id: str, job_id: str, applicant_id: str) -> Any:
                 }
             }
 
+        tags = await asyncio.get_running_loop().run_in_executor(
+            _executor,
+            lambda: get_supabase_admin_client()
+            .table("job_tags")
+            .select("*, tags(*)")
+            .eq("joblisting_id", job_id)
+            .execute(),
+        )
+
+        tags = [
+            tag["tags"]["name"]
+            for tag in tags.data
+            if "tags" in tag and "name" in tag["tags"]
+        ]
+
+        raw_score = 0.0
         if not parsed_resume:
             parsed_resume = {"raw_output": "No parsed resume available"}
+        else:
+            raw_output = parsed_resume.get("raw_output", {})
+            user_skills = raw_output.get("hard_skills", []) + raw_output.get(
+                "soft_skills", []
+            )
+            raw_score = cosine_similarity_scoring(user_skills, tags)
 
         prompt = (
             scoring_prompt
@@ -122,16 +144,18 @@ async def score_candidate(user_id: str, job_id: str, applicant_id: str) -> Any:
         )
 
         raw_output = scoring_gemini_model.generate_content(prompt).text.strip()
-
         if raw_output.startswith("```json"):
             raw_output = re.sub(r"```json|```", "", raw_output).strip()
+
+        raw_output = json.loads(raw_output)
+        raw_output["raw_score"] = float(round(raw_score, 2))
 
         inserted_id = await mongodb.insert_document(
             "scored_candidates",
             {
                 "user_id": user_id,
                 "job_id": job_id,
-                "score_data": json.loads(raw_output),
+                "score_data": raw_output,
             },
         )
 
@@ -155,7 +179,7 @@ async def score_candidate(user_id: str, job_id: str, applicant_id: str) -> Any:
 
         return {
             "message": "Candidate scored successfully",
-            "score_data": convert_objectid(json.loads(raw_output)),
+            "score_data": convert_objectid(raw_output),
         }
     except json.JSONDecodeError:
         raise HTTPException(status_code=500, detail="Model output was not valid JSON.")
